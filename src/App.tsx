e08d011b6f0babb10912b4825db931b35fb56872
codinghelpkp-sys <codinghelpkp@gmail.com>
2025-11-23 20:42:17 +0000

    Updated index.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="w-screen h-screen overflow-hidden flex flex-col">
       <header className="navbar">
-        <div className="navbar-container">
+        <div className="navbar-container navbar-glow">
           <div className="navbar-logo">
             <div className="logo-icon">D</div>
             <span className="logo-text">Dispulse</span>
@@ -10,16 +10,11 @@ function App() {
 
           <nav className="navbar-menu">
             <a href="#" className="nav-link active">Home</a>
-            <a href="#" className="nav-link">Features</a>
-            <a href="#" className="nav-link">About</a>
-            <a href="#" className="nav-link">Pricing</a>
-            <a href="#" className="nav-link">Blog</a>
+            <a href="#" className="nav-link">Services</a>
+            <a href="#" className="nav-link">Work</a>
+            <a href="#" className="nav-link">FAQ</a>
+            <a href="#" className="nav-link">Contact</a>
           </nav>
-
-          <div className="navbar-actions">
-            <button className="btn-login">Login</button>
-            <button className="btn-signup">Sign up</button>
-          </div>
         </div>
       </header>
 
